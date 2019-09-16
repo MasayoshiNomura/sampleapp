b14ec7d6ef263aa35d4672a504c51700318c304a
Masayoshi.Nomura <masayoshi.nomura1983@gmail.com>
2019-09-16 21:29:43 +0900

    add db connection config

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -22,7 +22,7 @@
 return array(
 	'default' => array(
 		'connection' => array(
-			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'      => 'mysql:host=mysql;dbname=default',
 			'username' => 'root',
 			'password' => 'root',
 		),
